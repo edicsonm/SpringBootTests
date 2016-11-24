5f1f9e37ac9048274a6c07c8d83828b35a8a98b3
Edicson Morales <edicson@gmail.com>
2016-11-24 19:32:42 +1100

    Merge branch 'master' of https://github.com/edicsonm/SpringBootTests

--- src/main/java/au/com/edimoto/mongo/Connect.java
+++ src/main/java/au/com/edimoto/mongo/Connect.java
@@ -1,7 +1,49 @@
 package au.com.edimoto.mongo;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static java.util.Arrays.asList;
+
 /**
  * Created by edicson on 23/11/16.
  */
 public class Connect {
+    public Connect() {
+
+
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("test");
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+        try {
+            db.getCollection("restaurants").insertOne(
+                    new Document("address",
+                            new Document()
+                                    .append("street", "2 Avenue")
+                                    .append("zipcode", "10075")
+                                    .append("building", "1480")
+                                    .append("coord", asList(-73.9557413, 40.7720266)))
+                            .append("borough", "Manhattan")
+                            .append("cuisine", "Italian")
+                            .append("grades", asList(
+                                    new Document()
+                                            .append("date", format.parse("2014-10-01T00:00:00Z"))
+                                            .append("grade", "A")
+                                            .append("score", 11),
+                                    new Document()
+                                            .append("date", format.parse("2014-01-16T00:00:00Z"))
+                                            .append("grade", "B")
+                                            .append("score", 17)))
+                            .append("name", "Vella")
+                            .append("restaurant_id", "41704620"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
